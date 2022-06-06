981b1ae51350546f15136d40c4548ea6a2a030b0
christumexora <christumexora@gmail.com>
2022-06-06 11:33:55 -0700

    Variable conditionals

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low ++)
+	{
+		putchar(low);
+	}
+	printf("\n");
+
+	return (0);
+}
