20aa3e3c28bc5de88c8c2ff18e82fe441ce851b0
christumexora <christumexora@gmail.com>
2022-06-06 12:08:20 -0700

    Variable conditionals

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,13 +11,13 @@ int main(void)
 	char lowerCase;
 	char upperCase;
 
-	for(lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
+	for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
 		putchar(lowerCase);
 
 	for (upperCase = 'A'; upperCase <= 'Z'; upperCase++)
 		putchar(upperCase);
 
-	putchar('\n');
+	putchar(i'\n');
 
 	return (0);
 }
