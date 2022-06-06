77dea2398b0c9efd6d6e89eeaa79dc11ed585eb5
christumexora <christumexora@gmail.com>
2022-06-06 12:34:32 -0700

    Variable conditionals

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,10 +9,10 @@
 int main(void)
 {
 	int alphabt;
-	
 	for (alphabt = 'a'; alphabt <= 'z'; alphabt++)
 		if (alphabt != 'e' && alphabt != 'q')
 			putchar(alphabt);
+	
 	putchar('\n');
 
 	return (0);
