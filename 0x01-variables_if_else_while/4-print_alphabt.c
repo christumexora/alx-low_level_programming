694b0901640cf4b2298de1e36bf6660a5704a27b
christumexora <christumexora@gmail.com>
2022-06-06 12:27:46 -0700

    Variable conditionals

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints all alphabets except e and q
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int alphabt;
+	
+	for (alphabt = 'a'; alphabt <= 'z'; alphabt++)
+
+		if (alphabt != 'e' && alphabt != 'q')
+			putchar(alphabt);
+	putchar('\n');
+
+	return (0);
+}
