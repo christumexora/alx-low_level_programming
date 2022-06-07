a826739f7b7f93d0f2f796337a8c6d64a12dbd1f
christumexora <christumexora@gmail.com>
2022-06-06 18:36:17 -0700

    Variable conditionals

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints all different combination of three digits with the list no.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int degit1, degit2, degit3;
+
+	for (degit1 = 0; degit1 <= 9; degit1++)
+	{
+		
+		for (degit2 = degit1 + 1; degit2 <= 9; degit2++)
+
+		{
+
+			for (degit3 = degit2 + 1; degit3 <= 9; degit3++)
+			{
+				putchar((degit1 % 10) + '0');
+					putchar((degit2 % 10) + '0');
+					putchar((degit3 % 10) + '0');
+
+				if (degit1 == 7 && degit2 == 8 && degit3 == 9)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}	
+	}
+
+	putchar('\n');
+
+	return (0);
+}
