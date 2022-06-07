c15e0d67c23a7ba16895a713ffb9d7158fe325c7
christumexora <christumexora@gmail.com>
2022-06-06 19:08:28 -0700

    Variable conditionals

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of two digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int dig1, dig2;
+
+	for (dig1 = 0; dig1 <= 98; dig1++)
+	{
+		for (dig2 = dig1 + 1; dig2 <= 99; dig2++)
+		{
+			putchar((dig1 / 10) + '0');
+			putchar((dig1 % 10) + '0');
+			putchar(' ');
+			putchar((dig2 / 10) + '0');
+			putchar((dig2 % 10) + '0');
+
+			if (dig1 == 98 && dig2 == 99)
+			{
+				continue;
+			}
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
