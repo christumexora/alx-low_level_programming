0c7aa40ccadad2a584a227d35e0de35ca7a05a4c
christumexora <christumexora@gmail.com>
2022-06-06 18:11:28 -0700

    Variable conditionals

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /*
- * main - prints all possible combinations of two digits in ascending order
+ * main - prints all possible combinations of two digits in ascending order sepa
  *
  * Return: Always 0 (Success)
  */
