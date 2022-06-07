f5b1f32bdb2a775e68067fce0a2d40cb148b49cc
christumexora <christumexora@gmail.com>
2022-06-06 17:59:18 -0700

    Variable conditionals

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of two digits in ascending order separately
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int degit1, degit2;
+
+	for (degit1 = 0; degit1 <= 9; degit++)
+	{
+		for (degit2 = degit1 + 1; degit2 <= 9; degit2++)
+		{
+			putchar((degit1 % 10) + '0');
+			putchar((degit2 % 10) + '0');
+
+			if (degit1 ==8 && degit2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
