c321c0bcb2d8353d6c347c994ed0841828b7287c
christumexora <christumexora@gmail.com>
2022-06-06 17:25:38 -0700

    Variable condiionals

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * int main - combination of numbers separated by a comma
+ * main - combination of numbers separated by a comma
  *
  * Return: Always 0 (Success)
  */
